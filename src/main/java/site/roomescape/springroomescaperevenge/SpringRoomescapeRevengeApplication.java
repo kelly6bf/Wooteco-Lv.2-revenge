e2e868c08b0595e9fcdb1423bc9927fa95a6d403
kelly6bf <kelly6bf@gmail.com>
2024-06-26 04:09:30 +0900

    init project

---
+++ src/main/java/site/roomescape/springroomescaperevenge/SpringRoomescapeRevengeApplication.java
@@ -0,0 +1,13 @@
+package site.roomescape.springroomescaperevenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRoomescapeRevengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRoomescapeRevengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/roomescape/springroomescaperevenge/SpringRoomescapeRevengeApplicationTests.java
@@ -0,0 +1,13 @@
+package site.roomescape.springroomescaperevenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRoomescapeRevengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
